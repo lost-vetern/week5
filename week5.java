7eec294d17e4d033c625840d929fb58d4008bb5e
shahzad <shazad.02@gmail.com>
2021-03-29 11:04:35 +0500

    3rd commit

---
+++ week5.java
@@ -0,0 +1,15 @@
+package week5;
+
+import javax.swing.*;
+
+
+public class week5 {
+    public static void main(String[] args) {
+        JFrame frame =new JFrame();
+        frame.setSize(300,300);
+        JLabel label =new JLabel("this is a label");
+        frame.getContentPane().add(label);
+        frame.show();
+    }
+    
+}
