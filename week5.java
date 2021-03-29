1fb3b3806b69ba2ee33fc9aba3cdec9457c27930
shahzad <shazad.02@gmail.com>
2021-03-29 11:08:55 +0500

    update1 1st commit

--- week5.java
+++ week5.java
@@ -7,7 +7,7 @@ public class week5 {
     public static void main(String[] args) {
         JFrame frame =new JFrame();
         frame.setSize(300,300);
-        JLabel label =new JLabel("this is a label");
+        JLabel label =new JLabel("this is a Update 1");
         frame.getContentPane().add(label);
         frame.show();
     }
